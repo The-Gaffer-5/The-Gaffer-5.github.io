7da0942d1e26bc713e93eebea83eb2b8d0f8492e
The-Gaffer-5 <51997952+The-Gaffer-5@users.noreply.github.com>
2020-01-31 14:16:28 -0500

    Portfolio 2.0 update

---
+++ precache-manifest.ff0dbe5e6c55692476922894c079a820.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62214b209ca35f93f0af8d75fdc9e9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b342a2f266bcb6382808",
+    "url": "/static/css/main.b9a9ed5f.chunk.css"
+  },
+  {
+    "revision": "9cd471bb8da6cfd26024",
+    "url": "/static/js/2.a11caf71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a11caf71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b342a2f266bcb6382808",
+    "url": "/static/js/main.347fe08c.chunk.js"
+  },
+  {
+    "revision": "bb277c7a2dad8740fd25",
+    "url": "/static/js/runtime-main.989530f5.js"
+  },
+  {
+    "revision": "b753bf787c20dc9c9500b1d67eac401c",
+    "url": "/static/media/arrow.b753bf78.svg"
+  },
+  {
+    "revision": "6605b0d72bd2c3e9420662ab0ac75cab",
+    "url": "/static/media/envelope.6605b0d7.png"
+  },
+  {
+    "revision": "4d923c6959adb5c178d96a328dc0b9d5",
+    "url": "/static/media/headcutout.4d923c69.png"
+  },
+  {
+    "revision": "270beaff809e2e16c18106b6d9ac12b3",
+    "url": "/static/media/logo.270beaff.svg"
+  },
+  {
+    "revision": "c754355e24e5b74862c2d9d94c13df91",
+    "url": "/static/media/prescott.c754355e.png"
+  }
+]);
